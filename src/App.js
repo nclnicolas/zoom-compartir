76b4eaab7aef4197db3233fbe358e9f9f3e000ed
Nicolas <ncl.enriquez@gmail.com>
2022-07-12 10:39:00 -0300

    prueba de zoom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
+import "./App.css";
 import React, { useEffect, useState } from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
-import Telecentro from "./assets/img/Telecentro.pdf"
-
+import Telecentro from "./assets/img/Telecentro.pdf";
 
 function App() {
+  const [numPages, setnumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+  const [files, setFiles] = useState(null);
 
-  const[numPages, setnumPages] = useState(null);
-  const[pageNumber, setPageNumber] = useState(1);
-  const[files, setFiles] = useState(null);
-
-  function onDocumentLoadSuccess({numPages}){
+  function onDocumentLoadSuccess({ numPages }) {
     setnumPages(numPages);
     setPageNumber(1);
   }
@@ -29,97 +27,91 @@ function App() {
   } */
 
   useEffect(() => {
-    async function getArchivo(){
+    async function getArchivo() {
       const blob = await Telecentro;
-      const titulo ='servicio_de_factura'
-      const files = new File([blob], `${titulo}.pdf`, {type: 'application/pdf'});
+      const titulo = "servicio_de_factura";
+      const files = new File([blob], `${titulo}.pdf`, {
+        type: "application/pdf",
+      });
       setFiles(files);
     }
     getArchivo();
-  }, [])
+  }, []);
 
-  const archivo ={
-    title: 'Web share con archivo',
-    text: 'Ademas de textos',
-    url: 'https://nico.com',
-    files:[files]
-  }
+  const archivo = {
+    title: "Web share con archivo",
+    text: "Ademas de textos",
+    url: "https://nico.com",
+    files: [files],
+  };
 
-  function shareAcross(objeto){
-    if(navigator.share){
+  function shareAcross(objeto) {
+    if (navigator.share) {
       navigator
-      .share(objeto)
-      .then(() => console.log('Excelente'))
-      .catch((err) => console.log(err))
-    }else{
-      console.log('No soportado');
+        .share(objeto)
+        .then(() => console.log("Excelente"))
+        .catch((err) => console.log(err));
+    } else {
+      console.log("No soportado");
     }
   }
 
-
-const [scale, setScale] = useState(1);
-const [pann, setPann] = useState(true);
-const [zoom, setZoom] = useState('reset');
-
+  const [scale, setScale] = useState(1);
+  const [pann, setPann] = useState(true);
+  const [zoom, setZoom] = useState("reset");
 
   return (
     <>
-      Holis
+      Holanda
       <TransformWrapper
-         centerOnInit
-         centerZoomedOut
-         initialScale={1}
-          panning={{disabled:pann, velocityDisabled: true}} //desactiva vista panoramica
-          doubleClick={{mode:zoom, step: scale}}
-           /* onZoom={() => console.log('onZoom')} */
-          /* onPanning={() => console.log('onPanning')} */
-          /* onPanningStop={() => console.log('onPanningStop')} */
-         /*  onPanningStart={() => console.log('onPanningStar')} */
-          /* onWheelStart={() => console.log('onWheelStar')} */
-          /* onWheelStop={() => console.log('onWheelStop')} */
-          onPanningStart={(e) => {
-            if(e.state.scale !== 1 && e.instance.setup.doubleClick.mode !== 'reset'){
-              setZoom('reset')
-              /* setScale(-1) */
-              setPann(false)
-              console.log('entro');
-            }else{
-              setZoom('zoomIn')
-              /* setScale(1) */
-              setPann(true)
-              console.log('salio');
-            }
-          }}
-          
-        >
-          <TransformComponent >
-      <div >
-      <Document className="document-scroll" file={Telecentro} onLoadSuccess={onDocumentLoadSuccess}>
-        {Array.from(
-          new Array(numPages),
-          (el, index) => (
-            <Page 
-            key={`page_${index+1}`}
-            pageNumber={index+1}
-          
-            />
-          )
-        )}
-      </Document>
-      </div>
-      </TransformComponent>
-        </TransformWrapper>
-          
-        <h4>Compartir Archivo</h4>
-        {/* <input type='file' multiple onChange={(e) => {setFiles(e.target.files)}} ></input>
+        centerOnInit
+        centerZoomedOut
+        initialScale={1}
+        panning={{ disabled: pann}} //desactiva vista panoramica
+        doubleClick={{ mode: zoom, step: scale }}
+        /* onZoom={() => console.log('onZoom')} */
+        /* onPanning={() => console.log('onPanning')} */
+        /* onPanningStop={() => console.log('onPanningStop')} */
+        /*  onPanningStart={() => console.log('onPanningStar')} */
+        /* onWheelStart={() => console.log('onWheelStar')} */
+        /* onWheelStop={() => console.log('onWheelStop')} */
+        onPanningStart={(e) => {
+          if (e.state.scale !== 1) {
+            setZoom("reset");
+            setScale(-1);
+            setPann(false);
+            console.log("entro");
+          } else {
+            setZoom("zoomIn");
+            setScale(1);
+            setPann(true);
+            console.log("salio");
+          }
+        }}
+      >
+        <TransformComponent>
+          <div>
+            <Document
+              className="document-scroll"
+              file={Telecentro}
+              onLoadSuccess={onDocumentLoadSuccess}
+            >
+              {Array.from(new Array(numPages), (el, index) => (
+                <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+              ))}
+            </Document>
+          </div>
+        </TransformComponent>
+      </TransformWrapper>
+      <h4>Compartir Archivo</h4>
+      {/* <input type='file' multiple onChange={(e) => {setFiles(e.target.files)}} ></input>
         <button onClick={() => {shareData()}}>Compartir</button> */}
-        <button onClick={() => shareAcross(archivo)}>Compartir</button>
+      <button onClick={() => shareAcross(archivo)}>Compartir</button>
     </>
   );
 }
 
 export default App;
-
 
 /* useEffect(() => {
     async function getArchivo(){
