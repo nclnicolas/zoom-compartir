f23652c47d260d0dd50d658a9eac4307f2db1c7c
Nicolas <ncl.enriquez@gmail.com>
2022-07-14 15:10:44 -0300

    comparte

--- src/App.js
+++ src/App.js
@@ -17,9 +17,10 @@ function App() {
   const shareData = async () => {
     if(navigator.share && navigator.canShare({files:file})){
       await navigator.share({
+        file,
         title: 'Hola buen dia',
         text: 'Hola hola',
-        files: file
+        
       })
     }else{
       console.log('No paso');
@@ -103,7 +104,7 @@ function App() {
           </div>
         </TransformComponent>
       </TransformWrapper>}
-      <h4>Compartir Archivo a travez de input</h4>
+      <h4>Compartir Archivo a travez de input!!</h4>
       <input type='file' multiple onChange={(e) => {setFile(e.target.files)}} ></input>
         <button onClick={() => {shareData()}}>Compartir</button>
         <h4>Compartir Archivo</h4>
